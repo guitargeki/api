559b525b75c041cf6f4182e0e559d8c98eedd2f3
Tommy <51040159+tommyquant@users.noreply.github.com>
2019-05-05 06:22:21 +1000

    Added initial geki data (not including results and Elo)

---
+++ database/scripts/50_data_match_statuses.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.match_statuses (title) VALUES 
+('Cancelled')
+,('In Progress')
+,('Judging')
+,('Completed')
+;
---
+++ database/scripts/52_data_events.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.events (title,date_start,date_end) VALUES 
+('Arrangement Competition','2018-11-01','2018-11-30')
+,('Christmas Edition','2018-12-01','2018-12-31')
+,('Elite Seat Challenges','2019-01-01','2019-01-31')
+,('Free-For-All Challenges','2019-02-01','2019-02-28')
+,('3-Gatsu / Shigatsu Arrangement','2019-03-01','2019-05-06')
+,('Ranked Battles (2019)','2019-03-01','2019-12-31')
+,('Miscellaneous', '2019-01-01', '2019-12-31')
+;
