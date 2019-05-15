559cd9a640db98ae2389fdd3fcce4fdc067eab77
Tommy <tommyquant@gmail.com>
2019-05-15 15:14:12 +1000

    Added checks for foreign keys
     - The new customJoi adds a foreignKeys object to the payload, which can be accessed in routes using request.payload.foreignKeys.
     - Resource class now contains pre handlers to check if a resource exists and check if all foreign keys point to an existing resource.
     - Added function to Model to check if a resource exists

--- api/src/v1/classes/Resource.js
+++ api/src/v1/classes/Resource.js
@@ -9,7 +9,56 @@ module.exports = class Resource {
      * @param {*} model 
      */
     constructor(model) {
+        this.pre = {};
         this.handlers = {};
+
+        // Pre handler to check if a resource exists. If it doesn't exist, it will return a 404.
+        this.pre.checkResourceExists = {
+            method: async function (request, h) {
+                const exists = await model.doesResourceExist(request.params.id);
+                if (!exists) {
+                    const error = Boom.notFound(codes[404]);
+                    return h.response(error.output.payload).code(error.output.statusCode).takeover();
+                }
+
+                return null;
+            },
+            assign: 'checkResourceExists',
+            failAction: 'error'
+        };
+
+        // Pre handler to check if all foreign keys point to an existing record. Return 422 if any keys point to a non-existing record.
+        this.pre.checkForeignKeysExist = {
+            method: async function (request, h) {
+                if (request.payload.foreignKeys) {
+                    const foreignKeys = request.payload.foreignKeys;
+
+                    for (let i = 0; i < foreignKeys.length; i++) {
+                        const id = foreignKeys[i].value;
+                        const tableName = foreignKeys[i].table;
+                        const key = foreignKeys[i].key;
+                        const exists = await model.doesResourceExist(id, tableName);
+
+                        if (!exists) {
+                            const error = Boom.badData(`${key} does not point to an existing resource`, foreignKeys[i].key);
+                            return h.response(error.output.payload).code(error.output.statusCode).takeover();
+                        }
+                    }
+
+                    delete request.payload.foreignKeys;
+                }
+
+                return null;
+            },
+            assign: 'checkForeignKeysExist',
+            failAction: 'error'
+        };
+
+        // Get one
+        this.handlers.getOne = async function (request, h) {
+            const data = await model.getOne(request.params.id);
+            return h.response(data).code(200);
+        };
 
         // Get list
         this.handlers.getList = async function (request, h) {
@@ -21,35 +70,15 @@ module.exports = class Resource {
         this.handlers.create = async function (request, h) {
             // ID of the new resource
             const id = await model.create(request.payload);
-    
+
             // Set 'Location' header to the URL for the new resource
             const response = h.response().code(201);
             response.header('Location', `${request.path}/${id}`);
             return response;
         };
 
-        // Get one
-        this.handlers.getOne = async function (request, h) {
-            // Return 404 if resource does not exist
-            const data = await model.getOne(request.params.id);
-            if (data === undefined) {
-                const error = Boom.notFound(codes[404]);
-                return h.response(error.output.payload).code(error.output.statusCode);
-            }
-    
-            // Return resource if found
-            return h.response(data).code(200);
-        };
-
         // Update
         this.handlers.update = async function (request, h) {
-            // Return 404 if resource does not exist
-            const data = await model.getOne(request.params.id);
-            if (data === undefined) {
-                const error = Boom.notFound(codes[404]);
-                return h.response(error.output.payload).code(error.output.statusCode);
-            }
-    
             await model.update(request.params.id, request.payload);
             return h.response().code(204);
         };
