b33a99f440a2888f404e8c8eb04ca238503abc67
Tommy <tommyquant@gmail.com>
2019-06-03 02:44:13 +1000

    Added waiting for config service

--- api-v1/src/common/config/index.js
+++ api-v1/src/common/config/index.js
@@ -1,9 +1,8 @@
 const fetch = require('node-fetch');
 
 async function init() {
-    const response = await fetch(`http://config/api-v1/${process.env.APP_ENV}`);
+    const response = await fetch(`http://config:3000/api-v1/${process.env.APP_ENV}`);
     const data = await response.json();
-    console.log(data);
     module.exports.vars = data;
 }
 
