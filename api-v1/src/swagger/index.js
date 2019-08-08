77fe3645e5200e186b6307ae1e85857b2c3a69d2
Tommy <51040159+tommyquant@users.noreply.github.com>
2019-08-08 17:32:34 +1000

    Update Swagger docs

--- api-v1/src/swagger/index.js
+++ api-v1/src/swagger/index.js
@@ -2,9 +2,11 @@ const basePath = '/v1';
 
 module.exports = {
     info: {
-        title: 'Guitargeki API V1 Documentation',
+        title: 'Guitargeki API v1',
         version: '1.0.0',
-        description: 'Documentation for the Guitargeki API. To use the POST and PATCH methods, provide a valid access token in the form of \'Bearer TOKEN\'.'
+        description: 'Guitargekis are competitive events where guitarists submit audio/video performances of a pre-chosen theme/song. At the end of each match or event, a panel of judges will vote for their favourite submission or rate each submission. The winner is the submission with the highest number of votes or rating.'
+            + '\n\nThis is the REST API developed to archive submissions and automatically rank participants. Features an Elo rating system, authentication and authorization using JWTs, rate limiting and auto-generated Swagger/OpenAPI 2.0 documentation and specification.'
+            + '\n\nAdmins: To use the **POST** and **PATCH** methods, provide a valid access token in the **Authorization** header. If you are using the endpoints directly on this page, provide a token in the **api_key** text box at the top-right of this page instead. Token must be in the form of `Bearer TOKEN`.'
     },
 
     basePath: basePath,
